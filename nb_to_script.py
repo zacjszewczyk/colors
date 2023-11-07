375b27f3be007f77b4a62f144ca2e55fcd02b9bf
Zac Szewczyk <zacjszewczyk@users.noreply.github.com>
2023-11-07 17:49:43 +0000

    Work

--- nb_to_script.py
+++ nb_to_script.py
@@ -6,18 +6,28 @@ import json
 
 # Checking if the script is the main program and not being imported
 if __name__ == "__main__":
+    # Basic input checking for required number of parameters 
     if len(sys.argv) != 2:
         print("Usage: python3 nb_to_script.py <file_path>")
         sys.exit(1)
     
+    # Basic input checking for required file path extension
     file_path = sys.argv[1]
-
     if (".ipynb" not in file_path):
         print("Error: nb_to_script.py requiures a Jupyter notebook as an input file.")
         sys.exit(1)
 
-    fd = open(file_path, "r")
+    # Clear the output file
+    open("output.py", "w").close()
+    script_fd = open("output.py", "a")
 
-    
-    
-    fd.close()
+    # Open the notebook file
+    notebook_fd = open(file_path, "r")
+
+    content = json.loads(notebook_fd.read())
+
+    print(content["cells"])
+
+    # Close the files
+    script_fd.close()
+    notebook_fd.close()
